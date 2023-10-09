358eaf9c71cd6a70339d04ff30ff875f6d9019c2
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-09 20:28:35 +0200

    Added spotify SDK and update auth

---
+++ src/server/auth/authOptions.ts
@@ -0,0 +1,95 @@
+import { type GetServerSidePropsContext } from "next";
+import {
+  getServerSession,
+  type DefaultSession,
+  type NextAuthOptions,
+  Account,
+} from "next-auth";
+
+import { env } from "@/env.mjs";
+import { JWT } from "next-auth/jwt";
+import spotifyProfile, { refreshAccessToken } from "./spotifyProfile";
+import { AuthUser, user } from "@/types/user";
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: DefaultSession["user"] & {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    };
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+/**
+ * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
+ *
+ * @see https://next-auth.js.org/configuration/options
+ */
+export const authOptions: NextAuthOptions = {
+  providers: [spotifyProfile],
+  session: {
+    maxAge: 60 * 60, // 1 hour
+  },
+  callbacks: {
+    async jwt({ token, account }: { token: JWT; account: Account | null }) {
+      if (!account) {
+        return token;
+      }
+      const updatedToken = {
+        ...token,
+        access_token: account?.access_token,
+        token_type: account?.token_type,
+        expires_at: account?.expires_at ?? Date.now() / 1000,
+        expires_in: (account?.expires_at ?? 0) - Date.now() / 1000,
+        refresh_token: account?.refresh_token,
+        scope: account?.scope,
+        id: account?.providerAccountId,
+      };
+      if (Date.now() < updatedToken.expires_at) {
+        return refreshAccessToken(updatedToken);
+      }
+      return updatedToken;
+    },
+    async session({ session, token }: { session: any; token: any }) {
+      const user: AuthUser = {
+        ...session.user,
+        access_token: token.access_token,
+        token_type: token.token_type,
+        expires_at: token.expires_at,
+        expires_in: token.expires_in,
+        refresh_token: token.refresh_token,
+        scope: token.scope,
+        id: token.id,
+      };
+      session.user = user;
+      session.error = token.error;
+      return session;
+    },
+  },
+  debug: env.NODE_ENV === "development",
+  secret: env.NEXTAUTH_SECRET,
+};
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
