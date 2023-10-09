7a3a27def2311a66652f5a9885b15d3a6a282165
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-09 20:28:52 +0200

    added global store

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,11 +3,22 @@ import { create } from "zustand";
 
 interface Store {
   stage: Stages;
+  file?: File;
+  trackIds: string[];
 
   setStage: (stage: Stages) => void;
+  setTrackIds: (trackIds: string[]) => void;
+  setFile: (file: File) => void;
+  removeFile: () => void;
 }
 
 export const useGlobalStore = create<Store>((set) => ({
   stage: "authentication",
+  file: undefined,
+  trackIds: [],
+
+  setTrackIds: (trackIds) => set({ trackIds }),
+  setFile: (file) => set({ file }),
   setStage: (stage) => set({ stage }),
+  removeFile: () => set({ file: undefined }),
 }));
