00ecd61cbaa630297dec68e0b411e20de4cbc7cf
Mauro Eijsenring <mauro@eijsenring.com>
2023-10-09 20:29:04 +0200

    added auth user type

---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export type AuthUser = {
+  name: string;
+  email: string;
+  image: string;
+  access_token: string;
+  token_type: string;
+  expires_at: number;
+  expires_in: number;
+  refresh_token: string;
+  scope: string;
+  id: string;
+};
